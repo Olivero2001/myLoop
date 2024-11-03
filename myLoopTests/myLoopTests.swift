c714ca955c6ae5795647969058d60a47411b4b2b
Olivero2001 <oliver.oeste@icloud.com>
2024-11-03 12:20:30 +0100

    Initial Commit

---
+++ myLoopTests/myLoopTests.swift
@@ -0,0 +1,17 @@
+//
+//  myLoopTests.swift
+//  myLoopTests
+//
+//  Created by Oliver Oeste on 03.11.24.
+//
+
+import Testing
+@testable import myLoop
+
+struct myLoopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
